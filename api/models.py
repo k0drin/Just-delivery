6f64d29b00a6a883deaeeb602b8db1027cfe739c
n1ckoli <canakryg971@gmail.com>
2024-04-11 01:55:57 +0400

    add test models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    # направление кухни: грузинская, украинская, армянская...
+    direction = models.CharField(max_length=255)
+    description = models.TextField
+    # для логирования
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    user_name = models.CharField(max_length=255)
+    user_id = models.IntegerField
+    user_email = models.EmailField
+    text = models.TextField
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
