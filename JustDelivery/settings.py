2ec9aa50f3a30c715c6abe623f89d4654204b595
Oleksandr Kosinov <canakryg971@gmail.com>
2024-08-06 10:09:16 +0400

    Feat: Added environment variable

--- JustDelivery/settings.py
+++ JustDelivery/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Redis
 REDIS_HOST = "localhost"
@@ -14,12 +16,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3x$tzsb=3qzcro#ygev@k-e!vqt&o95s3v$bb!9sp8&qo59+1b"
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- api/admin.py
+++ api/admin.py
@@ -3,17 +3,15 @@ from .models import *
 
 # admin.site.register(Category)
 
-
-# Представление модели в интерфейсе администратора
 class AdminCategory(admin.ModelAdmin):
     list_display = (
         "title",
         "description",
         "time_update",
         "is_published",
-    )  # Какие поля отображать в списке объектов
-    search_fields = ("title",)  # Поля для поиска
-    list_filter = ("time_create",)  # Фильтры по дате создания
+    )  
+    search_fields = ("title",)  
+    list_filter = ("time_create",)  
 
 
 admin.site.register(Category, AdminCategory)
